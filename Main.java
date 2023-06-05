51d602c384272305706f41d72f223bfb473f959a
jjsvs <135663430+jjsvs@users.noreply.github.com>
2023-06-05 16:45:53 -0400

    basic java classes
    initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Student {
+   int rollno;
+   String name;
+   int studentid;
+
+}
+
+class Employee {
+    int employee_id;
+    String name;
+    float salary;
+}
+
+class Teacher {
+    int id;
+    String name;
+    float salary;
+}
