ba86aacff4be937966431052f3fd948292abb7e4
jjsvs <135663430+jjsvs@users.noreply.github.com>
2023-06-15 10:52:14 -0400

    Add files via upload
    implementing the concepts of inheritance, encapsulation, and polymorphism.

--- Main.java
+++ Main.java
@@ -1,98 +1,65 @@
-class Student {
-    int rollNo;
-    String name;
-    int studentId;
-
-    // Input-Output method
-    int displayStudentDetails(int rollNo, int studentId) {
-        return rollNo;
-    }
-
-    // Input-No Output method
-    void setStudentDetails() {
-        int rollNo = 0;
-        String name = "";
-        int studentId = 0;
-        System.out.println("student details");
-    }
-
-    // No Input-Output method
-    int getStudentName() {
-        return rollNo;
-    }
-
-    // No Input-No Output method
-    void StudentDetails() {
-    }
-}
-
-class Employee {
-    int employeeId;
-    String name;
-    float salary;
-
-    // Input-Output method
-    float displayEmployeeId(int employeeId) {
-        return employeeId;
-    }
-
-    // Input-No Output method
-    void EmployeeDetails(int employeeId, String name, float salary) {
-        System.out.println("employee details");
-    }
-
-    // No Input-Output method
-    String getEmployeeName() {
-        return name;
-    }
-
-    // No Input-No Output method
-    void Employee() {
-    }
-}
-
-class Teacher {
+abstract class Person {
     int id;
     String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    abstract void displayDetails();
+}
+
+class Student extends Person {
+    int rollNo;
+
+    public Student(int id, String name, int rollNo) {
+        super(id, name);
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    void displayDetails() {
+        System.out.println("Student Name: " + name + ", Student Id: " + id + ", Roll No: " + rollNo);
+    }
+}
+
+class Employee extends Person {
     float salary;
 
-    // Input-Output method
-    String displayTeacherName(String name) {
-        return name;
+    public Employee(int id, String name, float salary) {
+        super(id, name);
+        this.salary = salary;
     }
 
-    // Input-No Output method
-    void TeacherDetails(int id, String name, float salary) {
-        System.out.println("Teacher Details");
+    @Override
+    void displayDetails() {
+        System.out.println("Employee Name: " + name + ", Employee Id: " + id + ", Salary: " + salary);
+    }
+}
+
+class Teacher extends Person {
+    float salary;
+
+    public Teacher(int id, String name, float salary) {
+        super(id, name);
+        this.salary = salary;
     }
 
-    // No Input-Output method
-    void getTeacherName() {
-        System.out.println("Teacher Name");
-    }
-
-    // No Input-No Output method
-    void resetTeacherDetails() {
+    @Override
+    void displayDetails() {
+        System.out.println("Teacher Name: " + name + ", Teacher Id: " + id + ", Salary: " + salary);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.rollNo = 10; // Initialize rollNo
-        student.name = "abnj"; // Initialize name
-        student.studentId = 12345; // Initialize studentId
+        Person student = new Student(12345, "abnj", 10);
+        Person employee = new Employee(100, "auy", 5000.0f);
+        Person teacher = new Teacher(1, "rdr", 6000.0f);
 
-        Employee employee = new Employee();
-        employee.employeeId = 100; // Initialize employeeId
-        employee.name = "auy"; // Initialize name
-        employee.salary = 5000.0f; // Initialize salary
-
-        Teacher teacher = new Teacher();
-        teacher.id = 1; // Initialize id
-        teacher.name = "rdr"; // Initialize name
-        teacher.salary = 6000.0f; // Initialize salary
-
-
+        student.displayDetails();
+        employee.displayDetails();
+        teacher.displayDetails();
     }
 }
