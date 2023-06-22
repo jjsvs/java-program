9c03aaf83a410ec58e2ad85bbdf28e6971f00d65
jjsvs <135663430+jjsvs@users.noreply.github.com>
2023-06-22 10:10:22 -0400

    Added Array implementation

--- Main.java
+++ Main.java
@@ -1,65 +1,17 @@
-abstract class Person {
-    int id;
-    String name;
-
-    public Person(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    abstract void displayDetails();
-}
-
-class Student extends Person {
-    int rollNo;
-
-    public Student(int id, String name, int rollNo) {
-        super(id, name);
-        this.rollNo = rollNo;
-    }
-
-    @Override
-    void displayDetails() {
-        System.out.println("Student Name: " + name + ", Student Id: " + id + ", Roll No: " + rollNo);
-    }
-}
-
-class Employee extends Person {
-    float salary;
-
-    public Employee(int id, String name, float salary) {
-        super(id, name);
-        this.salary = salary;
-    }
-
-    @Override
-    void displayDetails() {
-        System.out.println("Employee Name: " + name + ", Employee Id: " + id + ", Salary: " + salary);
-    }
-}
-
-class Teacher extends Person {
-    float salary;
-
-    public Teacher(int id, String name, float salary) {
-        super(id, name);
-        this.salary = salary;
-    }
-
-    @Override
-    void displayDetails() {
-        System.out.println("Teacher Name: " + name + ", Teacher Id: " + id + ", Salary: " + salary);
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Person student = new Student(12345, "abnj", 10);
-        Person employee = new Employee(100, "auy", 5000.0f);
-        Person teacher = new Teacher(1, "rdr", 6000.0f);
+        int[] numbers = {1, 2, 3, 4, 5};
 
-        student.displayDetails();
-        employee.displayDetails();
-        teacher.displayDetails();
+        System.out.println("Using a for loop:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("Using a while loop:");
+        int i = 0;
+        while (i < numbers.length) {
+            System.out.println(numbers[i]);
+            i++;
+        }
     }
 }
